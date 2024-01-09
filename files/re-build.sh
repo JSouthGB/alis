9cd4e817f08f128cc4920b5be6fd0393efc2691f
jsouthgb <justin.southern@groundblock.dev>
2024-01-09 07:01:19 -0500

    changes

--- files/re-build.sh
+++ files/re-build.sh
@@ -41,16 +41,6 @@ EOF
 echo "mount -a"
 mount -a
 
-echo "systemctl daemon-reload"
-systemctl daemon-reload
-
-#echo "rclone copy -P /mnt/fourt/laptop.backup.1/sddm-theme-corners/corners/ /usr/share/sddm/themes/corners/"
-#rclone copy -P /mnt/fourt/laptop.backup.1/sddm-theme-corners/corners/ /usr/share/sddm/themes
-
-#echo "## sddm ##"
-#sed -i 's/Numlock=none/Numlock=on/g' /usr/lib/sddm/sddm.conf.d/default.conf
-#sed -i 's/Current=/NCurrent=corners/g' /usr/lib/sddm/sddm.conf.d/default.conf
-
 echo "## sddm ##"
 sed -i 's/#greeter-setup-script=/greeter-setup-script=/usr/bin/numlockx on/g' /etc/lightdm/lightdm.conf
 
@@ -88,7 +78,7 @@ echo "rclone copy -P /mnt/fourt/laptop.backup.1/home/devu/.config/zsh/ /home/dev
 rclone copy -P /mnt/fourt/laptop.backup.1/home/devu/.config/zsh/ /home/devu/.config/zsh
 
 echo "rclone copy -P /mnt/fourt/laptop.backup.1/home/devu/.zshenv /home/devu/"
-rclone copy -P /mnt/fourt/laptop.backup.1/home/devu/.zshenv /home/devu/.zshenv
+rclone copy -P /mnt/fourt/laptop.backup.1/home/devu/.zshenv /home/devu/
 
 echo "## chmod .ssh dir and files ##"
 chmod 700 /home/devu/.ssh/
