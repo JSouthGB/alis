f94ab70b24ac91f0d717a98e675cdb2270eda285
jsouthgb <justin.southern@groundblock.dev>
2024-03-05 23:32:31 -0500

    kde changes

--- alis.sh
+++ alis.sh
@@ -1581,7 +1581,7 @@ function desktop_environment_gnome() {
 }
 
 function desktop_environment_kde() {
-    pacman_install "plasma-meta plasma-wayland-session kde-system-meta kde-utilities-meta kde-graphics-meta kde-multimedia-meta kde-network-meta"
+    pacman_install "plasma-desktop sddm-kcm kscreen kde-inotify-survey kcalc kwalletmanager koko kio-gdrive"
 }
 
 # function desktop_environment_xfce() {
